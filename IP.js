43bf8971df17b8ed2b8886a0d060ea93b24ea032
Hrystyna <yaskulskahrystyna@gmail.com>
2022-05-31 21:18:39 +0300

    Add some functions and fix mistakes

--- IP.js
+++ IP.js
@@ -7,6 +7,8 @@ class IP {
   }
 }
 
+const parseIP = (ad) => isValidV4(ad) ? createIPv4(ad) : isValidV6(ad) ? createIPv6(ad) : new Error ("This is not valid address");
+
 const isValidV4 = (address) => {
   if(address.includes('.') && address.includes('/')) {
     const re = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}$/;
@@ -21,8 +23,6 @@ const createIPv4 = (address) => {
   return new IP(...add);
 }
 
-const parseIP = (ad) => isValidV4(ad) ? createIPv4(ad) : isValidV6(ad) ? createIPv6(ad) : new Error ("This is not valid address");
-
 const ipToInt = ({ip}) => {
   const nums = ip.map(n => parseInt(n,10));
   if(nums.includes(NaN)) throw Error('Wrong IPv4 format');
@@ -36,11 +36,6 @@ const ipToBinary = ({ip}) => {
     bin.push(part);  
   }
   return bin.join('.');
-}
-
-const ipToString = ({ip}) => {
-  const str = ip.map(s => s.toString());
-  return str.join('.');
 }
 
 const ipToDotQoud = ({ip}) => {
@@ -150,6 +145,10 @@ const toBroadcast = ({ip, mask}) => {
   return broadIP.join('.');
 }
 
+const isValidV6 = (address) => {
+  return true;
+}
+
 const createIPv6 = (address) => {
   const add = address.split('/');
   const mask = add[1];
@@ -175,14 +174,15 @@ const ip6ToBinary = ({ip}) => {
   return bin.join(':');
 }
 
-const ipToColonHex = ({ip}) => {
+const ip6ToColonHex = ({ip}) => {
   return ip.join(':');
 }
 
 const BITS_V6 = 128;
 const BYTES_v6 = 16;
+const OCTETS_v6 = 32
 
-const mask6ToBinary = ({mask}) => {
+const prefixToBinary = ({mask}) => {
   let num = parseInt(mask, 10);
   let bin = '';
   if(num >= BITS_V6 || num < 1) throw new Error('Wrong mask');
@@ -197,15 +197,33 @@ const mask6ToBinary = ({mask}) => {
   return bin;
 }
 
-const maskToHex = ({mask}) => {
-  
+const prefixToColonHex = ({mask}) => {
+  let nums = '';
+  const bin = [];
+  const res = [];
+  for(let i = 1; i <= BITS_V6; i++) {
+    if(i <= mask) nums += '1';
+    else nums += '0';
+    if(i % 4 === 0) {
+      bin.push(nums);
+      nums = '';
+    }
+  }
+  for(let i = 0; i < 32; i++) {
+    let num = bin[i];
+    nums += parseInt(num, 2).toString(16);
+    if(i % 4 === 3) {
+      res.push(nums);
+      nums = '';
+    }
+  }
+  return res.join(':');
 }
-
 
 // const a = parseIP('220.255.255.255/8');
 // const binIP = ipToBinary(a);
 // const intIP = ipToInt(a);
-// const strIP = ipToString(a);
+// const dotip = ipToDotQoud(a);
 // const binMask = maskToBinary(a);
 // const mask = maskToDotQoud(a);
 // const network = getNetwork(a);
@@ -214,11 +232,12 @@ const maskToHex = ({mask}) => {
 // const loop = isLoopBack(a); 
 // const multi = isMulticast(a);
 // const broad = toBroadcast(a);
-// const isb = isBroadcast(a);
-// console.log(a, binIP, intIP, strIP, binMask, mask, network, priv, localip, loop, multi, broad, isb); 
-// const b = parseIP('1:f:ff:f2f:f:abf:0:188/64');
+//const isb = isBroadcast(a);
+//console.log(a, binIP, intIP, dotip, binMask, mask, network, priv, localip, loop, multi, broad, isb); 
+// const b = parseIP('1:f:ff:f2f:f:abf:0:188/54');
 // const intIP6 = ip6ToInt(b);
 // const bin6IP = ip6ToBinary(b);
-// const colon6 = ipToColonHex(b);
-// const mask6 = mask6ToBinary(b);
-// console.log(b, intIP6, bin6IP, colon6, mask6);
+// const colon6 = ip6ToColonHex(b);
+// const mask6 = prefixToBinary(b);
+// const m6 = prefixToColonHex(b);
+// console.log(b, intIP6, bin6IP, colon6, mask6, m6);
